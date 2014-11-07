6e786a722d234b5dae5b40d77b6017725a6c0b0e
jtxd <745127305@qq.com>
2014-11-07 16:34:17 +0800

    第八次提交

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("fsdfsdf");
 		System.out.println("aaaaaaaaaaaaaaaaaaaaa");
-		System.out.println("fsdfsd2");
+		System.out.println("fsdfsd8");
 		
 	}
 
